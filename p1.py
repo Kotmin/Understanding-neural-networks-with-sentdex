6139a952624f7fd750b25d8967baad3e98e594f1
kotmin <hotor22@gmail.com>
2021-07-21 23:55:48 +0200

    I made a err on the dot with a comma

--- p1.py
+++ p1.py
@@ -1,10 +1,10 @@
-inputs = [1,2,3,2.5]
+inputs = [1.0,2.0,3.0,2.5]
 weights1 = [0.2,0.8,-0.5,1.0]
-weights2 = [0.5,-0.91,0.26,-0,5]
+weights2 = [0.5,-0.91,0.26,-0.5]
 weights3 = [-0.26,-0.27,0.17,0.87]
 
-bias1 = 2
-bias2 = 3
+bias1 = 2.0
+bias2 = 3.0
 bias3 = 0.5
 
 
@@ -12,6 +12,6 @@ bias3 = 0.5
 
 
 output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] +inputs[3]*weights1[3] + bias1,
-          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] +inputs[3]*weights2[3] + bias2, #ztego chlopu wychodzi1.21 a mi 2.46
+          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] +inputs[3]*weights2[3] + bias2, 
           inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] +inputs[3]*weights3[3] + bias3]
 print(output)
