e533b0c4f4a31a7cb9c0c6b56d883958bb972d82
Kotmin <70173732+Kotmin@users.noreply.github.com>
2021-07-25 01:11:57 +0200

    second layer

--- p4.py
+++ p4.py
@@ -10,5 +10,12 @@ weights = [[0.2, 0.8, -0.5,1.0],
 
 biases = [2,3,0.5]
 
+weights2 = [[0.1,-0.14,0.5],
+            [-0.5,0.12,-0.33],
+            [-0.44,0.73,-0.13]]
+
+biases2 = [-1,2,-0.5]
+
+
 output = np.dot(inputs, np.array(weights).T) + biases #we're changeing a list of the lists to an np object called array, and using T method(transpose)
 print(output)
