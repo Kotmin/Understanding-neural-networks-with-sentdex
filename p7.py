cc621a29673d6f8513dd917126fe75aff328bb94
Kotmin <70173732+Kotmin@users.noreply.github.com>
2021-08-06 20:37:16 +0200

    bugfixing

--- p7.py
+++ p7.py
@@ -5,5 +5,7 @@ target_output = [1,0,0]
 
 
 loss = -(math.log(softmax_output[0])*target_output[0] +
-        (math.log(softmax_output[1])*target_output[2] +
-        (math.log(softmax_output[2])*target_output[2])
+        (math.log(softmax_output[1])*target_output[2]) +
+        (math.log(softmax_output[2])*target_output[2]))
+
+print(loss)
